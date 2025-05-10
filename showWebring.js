45355df5e8d8512e02d953e405d1ec492380c7fc
0xsy <enginkeremvural@gmail.com>
2025-05-10 23:37:48 +0300

    a

--- showWebring.js
+++ showWebring.js
@@ -2,37 +2,42 @@ var nameSelfWebring = "0xsy";
 var quoteSelfWebring = "🎀 Cuteness is a defense mechanism. Please do not engage.";
 var imgurlSelfWebring = "https://i.pinimg.com/736x/72/05/2d/72052d9d64d420b1453cfbc3cc0fdf5a.jpg";
 
-// Div olarak oluşturulmalı!
-var templateWebring = document.createElement("div");
-templateWebring.id = "templateWebring";
-templateWebring.innerHTML = `
-  <style>
-    #templateWebring {
-      width: 300px;
-      height: 164px;
-      margin: 20px auto;
-      background-image: url('https://webring.koinuko.pink/img/bg.png');
-      background-size: cover;
-      font-family: Verdana;
-      color: #452134;
-      text-align: center;
-      border: 2px solid #DD28A7;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      padding: 10px;
-    }
-    #webringdesc {
-      font-size: 18px;
-      font-weight: bold;
-      color: #BF3C8E;
-      line-height: 1.4;
-    }
-  </style>
+var templateWebring = document.createElement("selfinsertwebring");
+templateWebring.innerHTML = 
+<style>
+  #templateWebring {
+    width: 300px;
+    height: 164px;
+    margin: auto;
+    background-image: url('https://webring.koinuko.pink/img/bg.png');
+    background-size: cover;
+    font-family: Verdana;
+    color: #452134;
+    text-align: center;
+    border: 2px solid #DD28A7;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+  }
+
+  #webringdesc {
+    font-size: 18px;
+    font-weight: bold;
+    color: #BF3C8E;
+    line-height: 1.4;
+  }
+
+  #webringiconbox {
+    display: none;
+  }
+</style>
+
+<div id='templateWebring'>
   <div id='webringdesc'>
     <p>${quoteSelfWebring}</p>
   </div>
-`;
-
+</div>
+;
 document.getElementById("selfinsertwebring").appendChild(templateWebring);
