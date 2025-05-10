7f78a1972b78393a2921c2017c372f3a615d08f4
0xsy <enginkeremvural@gmail.com>
2025-05-10 23:36:47 +0300

    a

--- showWebring.js
+++ showWebring.js
@@ -2,32 +2,36 @@ var nameSelfWebring = "0xsy";
 var quoteSelfWebring = "🎀 Cuteness is a defense mechanism. Please do not engage.";
 var imgurlSelfWebring = "https://i.pinimg.com/736x/72/05/2d/72052d9d64d420b1453cfbc3cc0fdf5a.jpg";
 
+// Div olarak oluşturulmalı!
 var templateWebring = document.createElement("div");
+templateWebring.id = "templateWebring";
 templateWebring.innerHTML = `
-  <div id='templateWebring' style="
-    width: 300px;
-    height: 164px;
-    margin: 20px auto;
-    background-image: url('https://webring.koinuko.pink/img/bg.png');
-    background-size: cover;
-    font-family: Verdana;
-    color: #452134;
-    text-align: center;
-    border: 2px solid #DD28A7;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 10px;
-  ">
-    <div id='webringdesc' style="
+  <style>
+    #templateWebring {
+      width: 300px;
+      height: 164px;
+      margin: 20px auto;
+      background-image: url('https://webring.koinuko.pink/img/bg.png');
+      background-size: cover;
+      font-family: Verdana;
+      color: #452134;
+      text-align: center;
+      border: 2px solid #DD28A7;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 10px;
+    }
+    #webringdesc {
       font-size: 18px;
       font-weight: bold;
       color: #BF3C8E;
       line-height: 1.4;
-    ">
-      <p>${quoteSelfWebring}</p>
-    </div>
+    }
+  </style>
+  <div id='webringdesc'>
+    <p>${quoteSelfWebring}</p>
   </div>
 `;
 
