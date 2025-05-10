3723d05adbbc33ddd65e1b17689a9c3824829afa
0xsy <enginkeremvural@gmail.com>
2025-05-11 00:13:43 +0300

    Update showWebring.js

--- showWebring.js
+++ showWebring.js
@@ -1,25 +1,26 @@
-var templateWebring = document.createElement("selfinsertwebring");
-templateWebring.innerHTML = `
-  <style>
-    #templateWebring {
-      width: 300px !important;
-      text-align: center !important;
-      border: 2px solid #DD28A7 !important;
-      margin: auto !important;
-      background-image: url('https://webring.koinuko.pink/img/bg.png');
-      background-size: cover;
-      padding: 20px !important;
-      font-family: Verdana, sans-serif !important;
-      color: #452134 !important;
-      font-size: medium !important;
-      font-weight: bold !important;
-      box-shadow: 0 0 10px #DD28A7;
-      border-radius: 12px;
-    }
-  </style>
-  <div id="templateWebring">
-    🎀 Cuteness is a defense mechanism. Please do not engage.
-  </div>
-`;
-
-document.getElementById("selfinsertwebring").appendChild(templateWebring);
+<script>
+  var templateWebring = document.createElement("selfinsertwebring");
+  templateWebring.innerHTML = `
+    <style>
+      #templateWebring {
+        width: 300px !important;
+        text-align: center !important;
+        border: 2px solid #DD28A7 !important;
+        margin: auto !important;
+        background-image: url('https://webring.koinuko.pink/img/bg.png');
+        background-size: cover;
+        padding: 20px !important;
+        font-family: Verdana, sans-serif !important;
+        color: #452134 !important;
+        font-size: medium !important;
+        font-weight: bold !important;
+        box-shadow: 0 0 10px #DD28A7;
+        border-radius: 12px;
+      }
+    </style>
+    <div id="templateWebring">
+      🎀 Cuteness is a defense mechanism. Please do not engage.
+    </div>
+  `;
+  document.getElementById("selfinsertwebring").appendChild(templateWebring);
+</script>
